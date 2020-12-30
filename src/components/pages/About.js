92521051015e1d8670f0e3a7f8416e883487332d
Victor Andreescu <victor.andreescu@gmail.com>
2020-12-30 14:54:15 +0200

    update about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <Fragment>
       <h1>About this App</h1>
-      <p>App to search Github Users</p>
+      <p>App to search for Github Users</p>
       <p>Version: 1.0.0</p>
     </Fragment>
   );
